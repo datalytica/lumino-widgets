e76169f027c7b1e49b1bdedd3e88b3ce3c300df8
Naveen Michaud-Agrawal <naveen.michaudagrawal@gmail.com>
2021-04-03 11:11:49 -0400

    Update copyright

--- src/dashboardtabbar.ts
+++ src/dashboardtabbar.ts
@@ -1,10 +1,11 @@
 /*-----------------------------------------------------------------------------
-| Copyright (c) 2014-2017, PhosphorJS Contributors
+| Copyright (c) Naveen-Michaud-Agrawal
 |
 | Distributed under the terms of the BSD 3-Clause License.
 |
 | The full license is in the file LICENSE, distributed with this software.
 |----------------------------------------------------------------------------*/
+
 import {
   ArrayExt, each
 } from '@lumino/algorithm';
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 /*-----------------------------------------------------------------------------
-| Copyright (c) 2014-2017, PhosphorJS Contributors
+| Copyright (c) Naveen-Michaud-Agrawal
 |
 | Distributed under the terms of the BSD 3-Clause License.
 |
